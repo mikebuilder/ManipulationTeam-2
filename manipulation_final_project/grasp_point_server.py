e67586afc565bf6e98f0d6156502aeb9c2dacfe7
mikebuilder <michaelabadjiev@gmail.com>
2020-10-10 13:46:16 -0400

    added finding diametricall opposite point

--- manipulation_final_project/grasp_point_server.py
+++ manipulation_final_project/grasp_point_server.py
@@ -4,6 +4,7 @@ from numpy import *
 from manipulation_final_project.srv import GetGraspPoints,GetGraspPointsResponse
 from sensor_msgs import *
 import sensor_msgs.point_cloud2 as pc2
+from geometry_msgs.msg import *
 
 service_name = "get_grasp_points"
 
@@ -52,11 +53,32 @@ def get_grasp_points(get_grasp_points_request):
             closest = (p[0], p[1], p[2])
 
     print("closest point is: " + str(closest[0]) + ", "+ str(closest[1]) + ", " + str(closest[2]))
+    graspPoint1 = Point()
+    graspPoint1.x = closest[0]
+    graspPoint1.y = closest[1]
+    graspPoint1.z = closest[2]
 
     # find diametrically opposite point
+    # first find the true opposite, which can be found using midpoint formula, then find the closest point cloud point to that point
+    opposite = (2*COM[0]-closest[0],2*COM[1]-closest[1],closest[2])
+
+    # opposite is now the true diametrically opposite point, we will find the closest point in the point cloud to that point
+    trueOpposite = (0, 0, 0)
+    dist = 1000
+    for p in usefulPoints:
+        newDist = sqrt((opposite[0] - p[0]) ** 2 + (opposite[1] - p[1]) ** 2 + (opposite[2] - p[2]) ** 2)
+        if newDist < dist:
+            dist = newDist
+            trueOpposite = (p[0], p[1], p[2])
+
+    graspPoint2 = Point()
+    graspPoint2.x = trueOpposite[0]
+    graspPoint2.y = trueOpposite[1]
+    graspPoint2.z = trueOpposite[2]
+
 
     print("got grasp points, returning and exiting")
-    return GetGraspPointsResponse(None, None)
+    return GetGraspPointsResponse(graspPoint1, graspPoint2)
 
 if __name__ == '__main__':
     rospy.init_node(service_name + "_server")

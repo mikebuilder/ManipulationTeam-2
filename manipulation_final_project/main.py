0f73f28186095fa24d8532e3a06c088bc7906201
Andrew Euredjian <andrew_103@rocketmail.com>
2020-10-21 03:36:03 -0700

    Try some stuff and add note about moving gripper via publishing to topic

--- manipulation_final_project/main.py
+++ manipulation_final_project/main.py
@@ -21,10 +21,15 @@ def get_pose_from_cloud(cloud):
     graspPoints = getGraspPoints(cloud)
 
     pose = Pose()
-    pose.orientation.w = 1.0
     pose.position.x = graspPoints.com.x + 0.35
     pose.position.y = graspPoints.com.y
-    pose.position.z = 0
+    pose.position.z = 0.755
+
+    # manually picked quaternion values based on initial orientation state of robot
+    pose.orientation.x = -0.5
+    pose.orientation.y = 0.0
+    pose.orientation.z = 0.0
+    pose.orientation.w = 0.0
 
     return pose
 
@@ -35,6 +40,7 @@ if __name__ == '__main__':
 
     robot = mv.RobotCommander()
     rospy.loginfo(robot.get_group_names())
+    rospy.loginfo(robot.get_link_names()) # gets all links in the model regardless of group membership
 
     scene = mv.PlanningSceneInterface()
     moveGroup = mv.MoveGroupCommander(move_group_name)
@@ -52,12 +58,23 @@ if __name__ == '__main__':
     #moveGroup.stop()
 
     while not rospy.is_shutdown():
+        # =============================
+        # NOTE: Grippers can be moved by publishing to /panda/panda_finger1_controller/command
+        # and /panda/panda_finger2_controller/command. A value of 0.0 is fully closed and
+        # a value of ~0.04 is fully open for both fingers.
+        # =============================
+
         cloud = rospy.wait_for_message(camera_topic, PointCloud2, timeout = None)
         pose = get_pose_from_cloud(cloud)
-        rospy.loginfo(pose)
 
         moveGroup.set_pose_target(pose)
-        moveGroup.go(wait = True)
+        joint_goal = moveGroup.get_joint_value_target() # convert target pose into joint values - seems to move better
+
+        # manually adjust wrist and joint right before wrist
+        joint_goal[5] = pi
+        joint_goal[6] = pi/4 # wrist
+
+        moveGroup.go(joint_goal, wait = True) # execute joint goals (to use pose, remove joint_goal parameter)
         moveGroup.stop()
         moveGroup.clear_pose_targets()
 
